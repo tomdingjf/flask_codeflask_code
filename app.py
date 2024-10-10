97656349968b6c524f05c12846b7cc37ec0224d7
Ding <582261456@qq.com>
2024-10-10 08:48:21 +0800

    ding

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from pythonproject import create_app
-import time
-from worker import main
 
 '''
 输出环境 requirements.txt
@@ -8,6 +6,13 @@ from worker import main
 
 运行 requirements.txt 安装环境
     pip install -r requirements.txt
+
+sql输出 dump-day21-202410100821.sql 文件
+    选择数据库day21，day21 -> 工具 -> 转储数据库
+    
+sql文件执行 dump-day21-202410100821.sql
+    在DBever选择数据库执行脚本即可
+
 
 教学视频：
     https://www.bilibili.com/video/BV1vD421E7cC?p=2&spm_id_from=pageDriver&vd_source=9487a22c8e8addbcaaaade7004791ee5
